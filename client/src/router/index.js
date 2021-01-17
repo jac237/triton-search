86f347d37c6688e43bc7d4af9013d72db2c7509e
Jessie Cruz <jac237@ucsd.edu>
2021-01-17 12:17:11 -0800

    router/index.js: Add About & Feedback view routes.

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+
 import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+import Feedback from '../views/Feedback.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +12,16 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
+  },
+  {
+    path: '/feedback',
+    name: 'Feedback',
+    component: Feedback,
   },
 ];
 

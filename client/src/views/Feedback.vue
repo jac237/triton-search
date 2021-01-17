e951d61c55697da7e1789bd6009e06cb2f14355f
Jessie Cruz <jac237@ucsd.edu>
2021-01-17 12:20:45 -0800

    Feedback.vue: Initial Commit.

---
+++ client/src/views/Feedback.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="notification has-text-centered p-4">
+    <span class="icon is-large my-4">
+      <i class="fas fa-exclamation-triangle fa-7x"></i>
+    </span>
+    <p class="title my-3">COMING SOON</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Feedback',
+};
+</script>
+
+<style>
+
+</style>

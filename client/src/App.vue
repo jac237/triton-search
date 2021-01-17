07ce02f2e8cc44ad137882c47a4201a0bc577dc3
Jessie Cruz <jac237@ucsd.edu>
2021-01-17 12:08:58 -0800

    App.vue: Refactored Navbar information to seperate vue component.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,53 +1,22 @@
 <template>
   <div id="app">
-    <nav class="navbar is-dark pl-2 pr-2 pt-1 pb-1" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-          target="_blank"
-          rel="noopener nofollow"
-          title="Triton Search: Unofficial course searching app for UCSD">
-            <img
-              src="../public/images/icons8-shark-48.png"
-              alt="Triton Search Logo Image"
-              width="35px"
-              height="40px">
-          <div
-            class="is-size-4 ml-2"
-            style="font-family: 'Roboto', serif;">
-            triton search
-          </div>
-        </a>
-      </div>
-      <div class="navbar-menu">
-        <div class="navbar-end">
-          <a
-            class="navbar-item has-text-dark"
-            href="https://github.com/jac237/"
-            target="_blank"
-            rel="noopener nofollow"
-            title="Github">
-              <div style="font-size: 36px;">
-                <i class="fab fa-github" style="color: white"></i>
-              </div>
-          </a>
-          <a
-            class="navbar-item has-text-dark"
-            href="https://linkedin.com/in/jessieacruz/"
-            target="_blank"
-            rel="noopener nofollow"
-            title="LinkedIn">
-              <div style="font-size: 36px;">
-                <i class="fab fa-linkedin" style="color: white;"></i>
-              </div>
-          </a>
-        </div>
-      </div>
-    </nav>
+    <Navbar />
     <router-view/>
+    <Footer />
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+};
+</script>
 
 <style lang="scss">
 .navbar-item img {

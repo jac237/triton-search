ae7e767c96bf81a740b3ca0030f247b11b26b805
Jessie Cruz <jac237@ucsd.edu>
2021-03-03 21:46:32 -0800

    disclaimer: removed warning icon

--- client/src/components/Disclaimer.vue
+++ client/src/components/Disclaimer.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="disclaimer has-text-left">
-    <b-message type="is-warning" has-icon>
+    <b-message type="is-warning">
       <strong>Disclaimer</strong>:
       This website is simply for educational purposes and is
       <strong>NOT</strong> an official UCSD website.
@@ -8,7 +8,7 @@
       For the complete list of departments and courses,
       please visit the "UCSD General Catalog for 2020-21"
       <a href="http://ucsd.edu/catalog/front/courses.html" target="_blank" rel="noopener noreferrer">here</a>.
-      <em>Note:</em> Current production only works on desktop.
+      <em>Note:</em> Current production recommends using desktop version.
     </b-message>
   </section>
 </template>
